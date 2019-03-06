fbf94b793a0baebffe05117a974a4694f24782b0
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2019-03-06 13:16:58 -0500

    Playlist support added

--- src/commands.py
+++ src/commands.py
@@ -22,13 +22,13 @@ class CommandsCog(commands.Cog):
         await ctx.message.remove_reaction("\U000023F3", ctx.me) #hourglass not done
         await ctx.channel.purge(limit = n + 1)
         embed = discord.Embed(title = f'{ctx.message.author} purged {n} messages!', colour = discord.Colour(0xe7d066))
-        embed.set_footer(text = f"Messages purged at {datetime.datetime.now()}")
+        embed.set_footer(text = f"Messages purged at {datetime.datetime.now()}") # TODO round off seconds!
         await ctx.send(embed = embed)
 
     @commands.command()
     async def shutdown(self, ctx):
         '''Shuts down the bot.'''
-        if ctx.author.id == 176384928672514050:
+        if ctx.author.id == 176384928672514050: # TODO put ID in config file
             await ctx.message.add_reaction("\U0001F50C") #power plug emoji
             await self.bot.logout()
         else:
--- src/music.py
+++ src/music.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import youtube_dl
 from os import listdir
-import asyncio  #for song_queue
+from collections import deque
 
 
 class MusicCog(commands.Cog):
@@ -11,17 +11,16 @@ class MusicCog(commands.Cog):
         self.vc = None
         self.audio_streamer = None
         self.default_volume = 1.0
-        self.song_queue = asyncio.Queue() #maxsize = 0
-        #self.playing = False
+        self.queue = deque([])
 
     @commands.command()
     async def join(self, ctx):
         await self.joinChannel(ctx)
 
-    async def joinChannel(self, ctx):  #must this be its own method?
+    async def joinChannel(self, ctx):
         '''Join the invoking player's voice channel.'''
         try:
-            self.vc = await ctx.message.author.voice.channel.connect() #weird things happen when window is x'ed out
+            self.vc = await ctx.message.author.voice.channel.connect()
         except:
             print("Player already connected.")
 
@@ -31,35 +30,55 @@ class MusicCog(commands.Cog):
         if self.vc is not None:
             await self.vc.disconnect()
 
+
+    @commands.command()
+    async def queue(self, ctx): # TODO implement in discord in human-readable way, embed
+        '''Displays the queue.'''
+        await ctx.send(str(self.queue))
+        print(self.queue)
+
     @commands.command()
     async def play(self, ctx, url):
-        '''Plays a downloaded song via FFMPEG, downloading it via youtube_dl.'''
-        #songlist = listdir('..\\music_cache')
-        #print('\nSongs in music_cache: ', songlist, '\n')
-        # TODO check if the song is already downloaded
+        '''Play a song via url.'''
         await ctx.message.add_reaction("\U000023F3") #hourglass not done
-        path = await MusicCog.download(url)
         await self.joinChannel(ctx)
-        if self.vc.is_playing():
-            return await ctx.send('Player already in use.')
+        # TODO check if the song is already downloaded (maybe), faster caching? May fix download bug
+        path = await MusicCog.download(url)
+        self.queue.append(path)
+        print('Enqueued {}'.format(path))
+
+        # Begin playing if we aren't already.
+        if not self.vc.is_playing():
+            self.playNext(ctx)
+
+        await ctx.message.add_reaction("\U00002705") #white heavy check mark (green background in discord)
+        await ctx.message.remove_reaction("\U000023F3", ctx.me) #remove hourglass not done
+
+
+    def playNext(self, ctx): # TODO: Add optional path for downloaded stuff?
+        '''Streams the next enqueued path in self.queue.'''
+        if not self.queue:
+            print('The audio queue is empty.')
+            return
+
+        path = self.queue.popleft()
         self.audio_streamer = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(path), volume = self.default_volume)
-        self.vc.play(self.audio_streamer)
-        await ctx.message.remove_reaction("\U000023F3", ctx.me) #hourglass not done
-        await ctx.message.add_reaction("\U00002705") #white heavy check mark
+        self.vc.play(self.audio_streamer, after = lambda e : self.playNext(ctx)) # play the song
+        print('playing', path)
 
     @staticmethod
     async def download(url):
-        '''Downloads the video using ytdl, returns file path as a string.'''
-        ytdl_opts = {
+        #'''Downloads the video using ytdl. Returns file path as a string.'''
+        ytdl_opts = { #move to higher scope?
         'quiet' : True,
         #'logger' : 'the logger'
         'format' : 'bestaudio/best',
         'outtmpl' : '..\\music_cache\\%(title)s.%(ext)s',
         }
         with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
-            info_dict = ydl.extract_info(url)
-            print('Downloaded ', info_dict['title'] + '.webm successfully.')
-            return "..\\music_cache\\" + info_dict['title'] + '.webm'
+            info_dict = ydl.extract_info(url) # TODO BUG if streaming a song, and the same song is requested, error
+            print('Downloaded ', info_dict['title'] + ' successfully.')
+            return "..\\music_cache\\" + info_dict['title'] + '.' + info_dict['ext']
 
     @commands.command()
     async def pause(self, ctx):
@@ -71,6 +90,11 @@ class MusicCog(commands.Cog):
         self.vc.resume()
         await ctx.message.add_reaction("\U000025B6") #play button
 
+    @commands.command()
+    async def skip(self, ctx):
+        self.vc.stop()
+        await ctx.message.add_reaction("\U00002705") #white heavy check mark
+
     @commands.command(aliases = ["vol"])
     async def volume(self, ctx, input = None):
         if input is not None:
@@ -81,7 +105,7 @@ class MusicCog(commands.Cog):
                 print('Volume must be a float.')
                 return await ctx.message.add_reaction("\U00002753") #question mark
         else:
-            await ctx.send('Volume set at ' + str(int(self.audio_streamer.volume) * 100) + '.')
+            await ctx.send('Volume set to ' + int(self.audio_streamer.volume * 100) + '%.')#broken when below 100%
 
 
 def setup(bot):

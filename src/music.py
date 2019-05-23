b481d888c5afb11b7c6a434256623fd7036fad85
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2019-05-23 13:39:34 -0400

    Added a YTDLSource class.

--- src/music.py
+++ src/music.py
@@ -6,7 +6,7 @@ from enum import Enum
 import asyncio
 import functools
 
-class MusicActivity(commands.Cog):
+class MusicActivity():
     class Status(Enum):
         PLAYING = 1
         PAUSED = 2
@@ -37,25 +37,45 @@ class MusicActivity(commands.Cog):
         pass
 
 
-############################################
-class MusicCog(commands.Cog):
+class YTDLSource():
 
     ytdl_opts = {
         "default_search": "auto",
         "noplaylist": True,
-        'quiet' : True,
-        #'logger' : 'the logger'
-        'format' : 'bestaudio/best',
-        'outtmpl' : '..\\music_cache\\%(extractor)s-%(id)s-%(title)s.%(ext)s', #%(title)s.%(ext)s',
+        'quiet': True,
+        # 'logger' : 'the logger'
+        'format': 'bestaudio/best',
+        'outtmpl': '..\\music_cache\\%(extractor)s-%(id)s-%(title)s.%(ext)s',  # %(title)s.%(ext)s',
     }
+
+    def __init__(self, query):
+        self.query = query
+        await download(self.query)
+
+
+    @classmethod
+    async def download(cls, query):  # TODO BUG can't use characters windows doesn't like in file name.
+        '''(Searches for and) Downloads the video using ytdl. Returns file path as a string.'''
+        with YoutubeDL(MusicCog.ytdl_opts) as ydl:  # TODO add time limit? 2hr
+            try:
+                info_dict = ydl.extract_info(query)  # BUG if streaming a song, and the same song is requested, error. Also HTTP Errors.
+            except Exception as e:
+                print("An error occurred while trying to download the song:", e)
+            if "entries" in info_dict:  # may have to do with playlists?
+                info_dict = info_dict["entries"][0]
+            print('Downloaded ', info_dict['title'] + ' successfully.')  # raises HTTPerrors sometimes, says info_dict is referenced before initialization :-(
+            return ("..\\music_cache\\" + ydl.prepare_filename(info_dict), info_dict)
+
+
+
+class MusicCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
         self.vc = None
         self.audio_streamer = None
         self.default_volume = 0.5
-        self.queue = deque([]) # A queue with tuples: (path, title)
-        self.songinfo = {}
+        self.queue = deque() # A queue with TYDLSource objects
         self.now_playing_pane = MusicActivity(bot)
 
     @commands.command()
@@ -77,7 +97,7 @@ class MusicCog(commands.Cog):
 
     @commands.command()
     async def queue(self, ctx): # TODO just song title/link, may need info_dict. make it nice
-        '''Displays the queue.'''
+        '''Displays the song queue.'''
         embed = discord.Embed(title='Song Queue', colour=discord.Colour(0xe7d066)) #Yellow
         if len(self.queue) == 0:
             await ctx.send("Nothing is enqueued. Play a song with /play")
@@ -96,9 +116,7 @@ class MusicCog(commands.Cog):
         await ctx.message.add_reaction("\U000023F3") #hourglass not done
         await self.joinChannel(ctx)
 
-        # TODO check if the song is already downloaded (maybe), faster caching?
         path, info = await MusicCog.download(query)
-        self.songinfo[path] = info #a record of every songinfo in the path. TODO pickle this? idk
         self.queue.append(path)
         print('Enqueued {}'.format(path))
         # Begin playing if we aren't already.
@@ -108,7 +126,8 @@ class MusicCog(commands.Cog):
         await ctx.message.remove_reaction("\U000023F3", ctx.me)  # hourglass not done
         await ctx.message.add_reaction("\U00002705") #white heavy check mark (green background in discord)
 
-    def playNext(self, ctx): # TODO: Add optional path for downloaded stuff?
+
+    def playNext(self, ctx):
         '''Streams the next enqueued path in self.queue.'''
         if not self.queue:
             asyncio.run_coroutine_threadsafe(self.now_playing_pane.change_activity(MusicActivity.Status.STOPPED, None), self.bot.loop) #update the activity
@@ -120,18 +139,8 @@ class MusicCog(commands.Cog):
         asyncio.run_coroutine_threadsafe(self.now_playing_pane.change_activity(MusicActivity.Status.PLAYING, self.songinfo[path]), self.bot.loop) #update the activity
         print('playing', path)
 
-    @staticmethod
-    async def download(query): # TODO BUG can't use characters windows doesn't like in file name.
-        '''Downloads the video using ytdl. Returns file path as a string.'''
-        with YoutubeDL(MusicCog.ytdl_opts) as ydl: #TODO add time limit? 2hr/10hr
-            try:
-                info_dict = ydl.extract_info(query) # BUG if streaming a song, and the same song is requested, error. Also HTTP Errors.
-            except Exception as e:
-                print("An error occurred while trying to download the song:", e)
-            if "entries" in info_dict: #something random from the github that I guess is required (see below)
-                info_dict = info_dict["entries"][0]
-            print('Downloaded ', info_dict['title'] + ' successfully.') #TODO raises HTTPerrors sometimes, says info_dict is referenced before initialization :-(
-            return ("..\\music_cache\\" + ydl.prepare_filename(info_dict), info_dict)
+
+
 
     # @commands.command()
     # async def search(self, ctx, *, search : str):

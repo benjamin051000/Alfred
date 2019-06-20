3e9ddfe6cf8484d21d63565de44cff12b8fa76e3
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2019-06-19 21:21:20 -0400

    Hid reddit login credentials in config.ini.

--- src/commands.py
+++ src/commands.py
@@ -41,15 +41,23 @@ class Commands(commands.Cog):
         else:
             await ctx.send("You can't shut me down.")
 
+#cfload.configSectionMap("Owner Credentials")["owner_id"]
     @commands.command()
-    async def meme(self, ctx):
-        """Gets a random meme from reddit and posts it."""
-        r = praw.Reddit(client_id='2iDsXiLTxBul9w',
-                           client_secret='ZeoMYdHyHYMkEOXYDN7T7WRZyDs',
-                           user_agent='Alfred',
-                           username='benjamin051000',
-                           password='Myst3exile.') #TODO HIDE THIS! (config.ini)
-        sub = r.subreddit('bonehurtingjuice')
+    async def meme(self, ctx, subreddit='dankmemes'):
+        """Gets a random meme from reddit and posts it.
+        Specify a subreddit to get a post from (actually works with any sub). Default is r/dankmemes."""
+        #Load the reddit login and bot credentials from config.ini
+        credentials = cfload.configSectionMap("Reddit API")
+
+        r = praw.Reddit(client_id=credentials['client_id'],
+                           client_secret=credentials['client_secret'],
+                           user_agent=credentials['user_agent'],
+                           username=credentials['username'],
+                           password=credentials['password'])
+        try:
+            sub = r.subreddit(subreddit)
+        except Exception:
+            await ctx.message.add_reaction("\U0000274C") #Cross mark
         posts = sub.hot(limit=100)
         rand = random.randint(0, 100)
         for i, post in enumerate(posts):

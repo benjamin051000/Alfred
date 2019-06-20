8f0a91e9418d3db40c8b18847fb2537bd8245f5e
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2019-06-20 09:41:48 -0400

    Output of subprocess call goes to update_git_log.txt

--- src/commands.py
+++ src/commands.py
@@ -48,10 +48,13 @@ class Commands(commands.Cog):
         """Shuts down the bot, updates the repo, and restarts using start.sh."""
         if ctx.author.id == int(cfload.configSectionMap("Owner Credentials")["owner_id"]):
             await ctx.message.add_reaction("\U0001F50C") #power plug emoji
-            await self.bot.logout()
-            subprocess.call(['../update_git.sh'])
+            file = open('update_git_log.txt', 'w')
+            subprocess.call(['../update_git.sh'], stdout=file)
+            file.close()
         else:
             await ctx.message.add_reaction('\U0000274C') #Cross mark
+
+        await self.bot.logout()
 
     @commands.command()
     async def meme(self, ctx, subreddit='dankmemes'):

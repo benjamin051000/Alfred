a17375368ae6aeadb2c424272ea688105c4e6298
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2019-07-08 18:49:04 -0400

    Overhauled logger.py. Updated to discord.py 1.2.3.

--- src/logger.py
+++ src/logger.py
@@ -1,25 +1,39 @@
-import discord
-from discord.ext import commands
 import logging
 import datetime
 
-class Logger:#(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.setupLogger()
+#Use logger variable to log stuff
+#TODO abstract some of this. Maybe into a class, or a group of methods
 
-    def setupLogger(self):
-        '''Creates a log in INFO mode, overwrites file unless the day changes.'''
-        LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
-        logging.basicConfig( filename = "..\\logs\\{}.log".format(datetime.date.today()),
-        level = logging.INFO, format = LOG_FORMAT, filemode = "w" )
-        logger = logging.getLogger()
-        logger.info("Logger set up successfully.")
+class Logger:  #TODO add a guild tag in log_format. Figure it out later i guess
 
+    logger = None
 
+    @classmethod
+    def setup_logger(cls):
+        log_format = '%(levelname)s %(asctime)s - %(message)s'
+        logging.basicConfig(filename='../logs/{}.log'.format(datetime.date.today()),
+                            level=logging.INFO,
+                            format=log_format,
+                            filemode='w')
+        cls.logger = logging.getLogger()
+        cls.logger.info('Logger set up successfully.')
 
+    @classmethod
+    def debug(cls, msg):
+        cls.logger.debug(msg)
 
+    @classmethod
+    def info(cls, msg):
+        cls.logger.info(msg)
 
-def setup(bot):
-#     bot.add_cog(Logger(bot))
-    pass
+    @classmethod
+    def warning(cls, msg):
+        cls.logger.warning(msg)
+
+    @classmethod
+    def error(cls, msg):
+        cls.logger.error(msg)
+
+    @classmethod
+    def critical(cls, msg):
+        cls.logger.critical(msg)

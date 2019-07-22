2bc7c561cbbd1f7c718ebaab781655c0e75d1662
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2019-07-21 20:47:13 -0400

    Updated update.sh

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #TODO /shutdown alfred before committing. May cause some funky issues with voiceclients.
-killall -v python3 main.py
+killall -9 "python3 main.py"
 git pull
 cd ..
 ./start.sh

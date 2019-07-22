93d8d361e660c092ba8174d4fbb38172b32b2d36
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2019-07-21 20:19:18 -0400

    Added output for debugging

--- deploy/deploy-server.js
+++ deploy/deploy-server.js
@@ -5,23 +5,28 @@ let app = express();
 
 let githubUsername = 'benjamin051000';
 
+app.use(express.json());
+
 app.listen(3000, ()=>{});
+
+
 
 app.post('/webhooks/github', (request, response) => {
 
     let sender = request.body.sender;
     let branch = request.body.ref;
+    console.log(sender, branch);
 
     if(branch.indexOf('master') >= 0 && sender.login === githubUsername) {
         //Update the server
         childP.exec('./update.sh', (err, stdout, stderr) => {
-        if(err) {
-            console.err(err);
-            //Send an error!
-            return response.send(500);
-        }
+            if(err) {
+                console.error(err);
+                //Send an error!
+                return response.send(500);
+            }
+        });
         //Send a success
         res.send(200);
-    });
     }
 });

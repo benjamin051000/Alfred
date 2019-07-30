f7929d0384beac3a61e2cae8f0a243f7ca8c562b
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2019-07-30 12:49:59 -0400

    Update packages during deployment restart.

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-#TODO /shutdown alfred before committing. May cause some funky issues with voiceclients.
+# Recommended: Use /shutdown alfred before committing. May cause some funky issues with voiceclients.
 killall -9 python3
 git pull
 cd ..
+pip3 install -r requirements.txt
 ./start.sh

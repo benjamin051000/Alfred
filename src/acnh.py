8ab9b99542f87f8d42c98ce4b05d6cb2a9507deb
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2020-03-30 16:27:33 -0400

    Added embed generator for fish info command. /fishinfo is now good enough for release (but still can be glitchy depending on API inconsistencies and messy code).

--- src/acnh.py
+++ src/acnh.py
@@ -1,7 +1,9 @@
-from discord.ext import commands
-import requests
 import re
+
+import discord
 import mwparserfromhell as mwp
+import requests
+from discord.ext import commands
 
 
 # Use https://www.mediawiki.org/wiki/API:Revisions#Sample_Code to GET from https://animalcrossing.fandom.com/wiki/Fish_(New_Horizons)
@@ -56,11 +58,56 @@ class ACNH(commands.Cog):
             await ctx.message.add_reaction('\U00002754')  # TODO remove, or make same color as above reaction (2753)
             return
 
-        # try:
-        await ctx.send(results[0])  # TODO send as an embed.
-        await ctx.send(pic_url)
-        # except IndexError:
-        #     await ctx.message.add_reaction('\U00002753')
+        # Create the embed
+        fish_name = results[0].params[1].strip(' \n[]')
+        fish_price = str(results[0].params[3].strip(' \n')) + ' bells'
+        fish_shadow_size = results[0].params[5].strip(' \n')
+
+        fish_tod = str(results[0].params[6])
+        clean = re.compile('<.*?>')
+        fish_tod = re.sub(clean, '', fish_tod)
+        fish_location = str(results[0].params[4]).strip(' \n') + ', ' + fish_tod
+
+        fish_north_times = results[0].params[7:19]
+        fish_south_times = results[1].params[7:19]
+        fish_north_months = ''
+        fish_south_months = ''
+
+        months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        n_num = 0
+        s_num = 0
+        for n in range(len(fish_north_times)):
+            if fish_north_times[n].value.strip(' \n') == '✓':
+                fish_north_months += months[n] + ', '
+                n_num += 1
+            if fish_south_times[n].value.strip(' \n') == '✓':
+                fish_south_months += months[n] + ', '
+                s_num += 1
+
+        fish_north_months = fish_north_months[:-2]
+        fish_south_months = fish_south_months[:-2]
+
+        if n_num == 12:
+            fish_north_months = 'All year'
+
+        if s_num == 12:
+            fish_south_months = 'All year'
+
+        embed = discord.Embed(title=fish_name,
+                              colour=discord.Colour(0x44b9e3))  # , url="fish_url")  # TODO add url to fish page
+        embed.set_thumbnail(url=pic_url)
+        embed.set_footer(text="Animal Crossing: New Horizons",
+                         icon_url="https://vignette.wikia.nocookie.net/animalcrossing/images/6/64/Favicon.ico/revision/latest?cb=20141121212537")  # TODO icon broken
+
+        embed.add_field(name="Value", value=fish_price, inline=True)
+        embed.add_field(name="Location, TOD", value=fish_location, inline=True)
+        embed.add_field(name="Shadow size", value=fish_shadow_size, inline=False)
+        embed.add_field(name="Northern Hemisphere", value=fish_north_months, inline=True)
+        embed.add_field(name="Southern Hemisphere", value=fish_south_months, inline=True)
+        # embed.add_field(name="Description",
+        #                 value="The blue marlin (力ジキマグロ, swordfish?), known as the swordfish in Animal Forest e+, is a rare fish in the Animal Crossing series. In Animal Forest e+ it is only seen in the sea around the Private Island, where it makes occasional appearances as an arapaima-sized shadow. However, due to the current of the island, some are impossible to catch as they are too far out to reach. The fish can be found all day and has a huge size.")
+
+        await ctx.send('This feature is currently in beta. Please be patient with glitches and errors.', embed=embed)
 
 
 def setup(bot):

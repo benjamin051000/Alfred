1a2a764cc9b14e289a14f085955d50b5a8246502
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-01-16 18:35:52 -0500

    removed deploy server (it was kinda bad)

--- deploy/update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Recommended: Use /shutdown alfred before committing. May cause some funky issues with voiceclients.
-killall -9 python3
-git pull
-cd ..
-pip3 install -r requirements.txt
-./start.sh
